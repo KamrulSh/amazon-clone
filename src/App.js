4acfdd2a49ad28d12bd67ba0379e43f16ec5d42c
Kamrul Islam Shahin <44506464+shahin-cuet@users.noreply.github.com>
2020-09-30 22:11:40 +0600

    add background image

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Header'
+import Home from './Home'
 
 function App() {
   return (
     <div className="app">
-      <Header/>
+      <Header />
+      <Home />
     </div>
   );
 }
