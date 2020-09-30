e2875461cb422de1ea8e82cec819a648f5456401
Kamrul Islam Shahin <44506464+shahin-cuet@users.noreply.github.com>
2020-09-30 22:15:17 +0600

    change to css BEM convention

--- src/Header.js
+++ src/Header.js
@@ -8,32 +8,32 @@ function Header() {
     return (
         <div className='header'>
 
-            <img className='header_logo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"></img>
+            <img className='header__logo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"></img>
 
-            <div className='header_search'>
-                <input className='header_searchInput' type='text' />
-                <SearchIcon className='header_searchIcon'></SearchIcon>
+            <div className='header__search'>
+                <input className='header__searchInput' type='text' />
+                <SearchIcon className='header__searchIcon'></SearchIcon>
             </div>
 
-            <div className='header_nav'>
-                <div className='header_option'>
-                    <span className='header_optionLineOne'>Hello Guest</span>
-                    <span className='header_optionLineTwo'>Sign In</span>
+            <div className='header__nav'>
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>Hello Guest</span>
+                    <span className='header__optionLineTwo'>Sign In</span>
                 </div>
 
-                <div className='header_option'>
-                    <span className='header_optionLineOne'>Returns</span>
-                    <span className='header_optionLineTwo'>& Orders</span>
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>Returns</span>
+                    <span className='header__optionLineTwo'>& Orders</span>
                 </div>
 
-                <div className='header_option'>
-                    <span className='header_optionLineOne'>Your</span>
-                    <span className='header_optionLineTwo'>Prime</span>
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>Your</span>
+                    <span className='header__optionLineTwo'>Prime</span>
                 </div>
 
-                <div className='header_optionBasket'>
+                <div className='header__optionBasket'>
                     <ShoppingBasketIcon />
-                    <span className='header_optionLineTwo header_basketCount'>0</span>
+                    <span className='header__optionLineTwo header__basketCount'>0</span>
                 </div>
             </div>
         </div>
