6a1fe0580b18dcab1b97a364a45156df8a6effc3
Kamrul Islam Shahin <44506464+shahin-cuet@users.noreply.github.com>
2020-10-02 00:19:55 +0600

    push item into the data layer

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import './Product.css'
+import { useStateValue } from './StateProvider'
 
 function Product({ id, title, image, price, rating }) {
+    const [{basket}, dispatch] = useStateValue();
+    console.log("This is basket ==>", basket);
+    const addToBasket = () => {
+        // dispatch the item into data layer
+        dispatch({
+            type: "ADD_TO_BASKET",
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            }
+        });
+    };
+
     return (
         // display product info, price, image, rating and add to cart in home section
         <div className="product">
@@ -21,7 +38,7 @@ function Product({ id, title, image, price, rating }) {
             <div>
                 <img src={image} alt=""></img>
             </div>
-            <button>Add to basket</button>
+            <button onClick={addToBasket}>Add to basket</button>
 
         </div>
     )
