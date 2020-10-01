d1a1422922d6d84d14854f66f976234ce563eeb5
Kamrul Islam Shahin <44506464+shahin-cuet@users.noreply.github.com>
2020-10-01 23:54:18 +0600

    global state management with React hooks

---
+++ src/StateProvider.js
@@ -0,0 +1,15 @@
+import React, { createContext, useContext, useReducer } from 'react'
+
+// prepares the data layer
+export const StateContext = createContext();
+
+// wrap the app and provide the data layer
+export const StateProvider = ({ reducer, initialState, children }) => (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+        {children}
+    </StateContext.Provider>
+);
+
+// pull the information from the data layer
+export const useStateValue = () => useContext(StateContext);
+
